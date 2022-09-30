fad87efe9e87fd1b6a5a3de9d8636263521e817d
prathapreddybattula <112234878+prathapreddybattula@users.noreply.github.com>
2022-09-30 11:03:09 +0530

    Create create location.cpp

---
+++ create location.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+  
+int main(){
+    int input[100], count, i, num;
+    cout << "Enter Number of Elements in Array\n";
+    cin >> count;
+    cout << "Enter " << count << " numbers \n";
+    for(i = 0; i < count; i++){
+        cin >> input[i];
+    }
+    cout << "Enter a number to serach in Array\n";
+    cin >> num;
+    for(i = 0; i < count; i++){
+        if(input[i] == num){
+            cout << "Element Location " << i+1;
+            break;
+        }
+    }
+    if(i == count){
+        cout  << "Element Not Present in Input Array\n";
+    }
+
+    return 0;
+}
